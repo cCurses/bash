fe4feb1ca138320f829d386ecdfb0d716ced8ee5
cCurses <cCureses@runbox.com>
2014-08-31 06:35:57 +0200

    rc3

--- lvmluks.sh
+++ lvmluks.sh
@@ -15,22 +15,17 @@
 
 
 if [ "$(whoami)" != 'root' ]; then
-        echo 'You need to be root.'
+        echo 'Run as root.'
         exit 1;
 fi
-echo -e 'Select the disk you want to prepare for luks and lvm'
-	ls -l /dev/[sh]d[a-z]
-	read -e disk
-echo -e 'Would you like to fill the disk with random data?\nNOTE: This will take a while! But offers better cryptoanalytical integrity.\nY/N?'
-	read = fill
-if [ fill = 'y' ]; then
+ls -l /dev/[sh]d[a-z]
+read -e -p 'Select the disk you want to prepare for luks and lvm: ' disk
+read -p 'Would you like to fill the disk with random data? [Y/N]' fill
+if [ fill = 'y' ];
+then
 	dd if=/dev/urandom of=$disk
 fi
-echo -e '\nNow we will use fdisk to partition the drive and create 2 partitions.'
-echo -e '1st partition will be used as /boot and will not be encrypted.'
-echo -e '2nd partition will be the remainder of the disk, and we will use this for the lvm encrypted part.\n'
-	read -n1 -r -p "Press any key to continue..."
-
+read -n1 -r -p "Ready to partition the disk. Press any key to continue..."
 fdisk $disk <<EOF
 d
 4
@@ -58,46 +53,41 @@ t
 w
 EOF
 clear
-
 fdisk $disk -l
 echo -e '\nDone partitioning\nLets encrypt some 1s and 0s\n'
-
 part='2'
 diskpart=$disk$part
-
 cryptsetup -s 256 -y luksFormat $diskpart
 sleep 2
 cryptsetup luksOpen $diskpart crypt
 pvcreate /dev/mapper/crypt
-echo -e '\nWould you like to set your own name on the volume group?\nIf not, i will create one called vgcrypt\nY/N?'
-	read -e askvg
-if [ $askvg = 'y' ]; then
-	echo -e 'Enter then name of your new volume group.'
-	read volumename
+read -p 'Would you like to name your volume group? Default: vgenc [Y/N]' askvg
+if [ $askvg = 'y' ];
+then
+	read -p 'Enter then name of your new volume group: ' volumename
 		vgcreate $volumename /dev/mapper/crypt
 fi
-echo -e Chose your root partition size. eg. 10G/1024M
-	read -r root
-		lvcreate -L $root -n root vgenc
-echo -e Chose your home partition size. eg. 10G/1024M
-	read -r home
-		lvcreate -L $home -n home vgenc
-echo -e Chose your swap partition size. eg. 10G/1024M
-	read -r swap
-		lvcreate -L $swap -n swap vgenc
-echo -e '\nWould you like to create additional partitions?\nY/N'
-read -e ask
-while [ $ask = 'y' ]; do
-	echo -e 'Enter the mountpoint of your partition. eg. var'
-		read -e newpart
-	echo -e 'Enter your '$newpart' partition size. eg. 10G/1024M'
-		read -e newsize
-		lvcreate -L $newsize -n $newpart vgenc
-	echo -e 'Add more?\nY/N'
-		read -e ask
+read -p 'Chose your root partition size. eg. 10G/1024M: ' root
+	lvcreate -L $root -n root vgenc
+read -p 'Chose your home partition size. eg. 10G/1024M: ' home
+	lvcreate -L $home -n home vgenc
+read -p 'Chose your swap partition size. eg. 10G/1024M: ' swap
+	lvcreate -L $swap -n swap vgenc
+read -p 'Would you like to create additional partitions? [Y/N]' ask
+while [ $ask = 'y' ];
+do
+	read -p 'Enter the mountpoint of your partition. eg. var: ' newpart
+	read -p 'Enter your '$newpart' partition size. eg. 10G/1024M: ' newsize
+	lvcreate -L $newsize -n $newpart vgenc
+	read -p 'Add more? [Y/N]' ask
 done
 vgscan --mknodes
 vgchange -ay
-mkswap /dev/vgenc/swap
+if [ -e '/dev/vgenc/' ];
+then
+	mkswap /dev/vgenc/swap
+else
+	mkswap /dev/$volumename/swap
+fi
 cryptsetup luksDump $disk
 echo -e '\nAll done.\nYou can now run setup.'
